81081db374523e18d0138e276e8fd6d5846ced93
Ladder Theory <brenton.ferullo@me.com>
2021-03-24 08:55:04 -0400

    added problem 12

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
-use core::f32;
-use std::{io::{Write}, collections::HashMap};
-use num::{Num, NumCast, FromPrimitive};
+use std::{collections::HashMap, io::{Write}, u128};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() {
 	let mut euler: HashMap<usize, (String, Box<dyn Fn() -> String>)> = HashMap::new();
 	euler.insert(
 		1,
-		(//1
+		(
 			String::from("Find the sum of all the multiples of 3 or 5 below 1000."),
 			Box::new(|| {
 				let mut sum = 0;
@@ -23,7 +21,7 @@ fn main() {
 	);
 	euler.insert(
 		2,
-		(//2
+		(
 			String::from("By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."),
 			Box::new(|| {
 				let mut fib = vec![1,2];
@@ -47,7 +45,7 @@ fn main() {
 	);
 	euler.insert(
 		3,
-		(//3
+		(
 			String::from("What is the largest prime factor of the number 600851475143?"),
 			Box::new(|| {
 				let num: u64 = 600851475143;
@@ -70,7 +68,7 @@ fn main() {
 	);
 	euler.insert(
 		4,
-		(//4
+		(
 			String::from("Find the largest palindrome made from the product of two 3-digit numbers."),
 			Box::new(|| {
 				let mut r = 0;
@@ -114,7 +112,7 @@ fn main() {
 	);
 	euler.insert(
 		5,
-		(//5
+		(
 			String::from("What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"),
 			Box::new(|| {
 				let mut r;
@@ -146,7 +144,7 @@ fn main() {
 	);
 	euler.insert(
 		7,
-		(//7
+		(
 			String::from("What is the 10,001st prime number?"),
 			Box::new(|| {
 				let mut primes = vec![2];
@@ -174,12 +172,12 @@ fn main() {
 				}
 
 				(*primes.last().unwrap()).to_string()
-			}),
+			})
 		)
 	);
 	euler.insert(
 		8,
-		(//8
+		(
 			String::from("Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?"),
 			Box::new(|| {
 				let bignum = String::from("7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450");
@@ -202,12 +200,12 @@ fn main() {
 				}
 				
 				(largest.1).to_string()
-			}),
+			})
 		)
 	);
 	euler.insert(
 		9,
-		(//9
+		(
 			String::from("There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product abc."),
 			Box::new(|| {
 				let find_k = |h,s| {//snagged this formula from https://www.youtube.com/watch?v=QJYmyhnaaek
@@ -217,8 +215,6 @@ fn main() {
 					// a + b + c = 2(h^2 hk) = s
 					// formula to find k from h & s:
 					// -h + (s/2h) = k
-
-
 					(0-h) + (s/(2*h))
 				};
 
@@ -232,8 +228,6 @@ fn main() {
 				};
 
 				let mut hk: Vec<i32> = Vec::new();
-
-				let mut r: i32= 0;
 
 				let sum = 1000;
 
@@ -252,12 +246,12 @@ fn main() {
 				}
 
 				find_product(hk[0],hk[1]).to_string()
-			}),
+			})
 		)
 	);
 	euler.insert(
 		10,
-        (//10
+        (
             String::from("Find the sum of all the primes below two million."),
             Box::new(|| {
                 let mut primes = (2..2000000).collect::<Vec<u64>>();
@@ -276,7 +270,47 @@ fn main() {
 				}
 
 				sum.to_string()
-            }),
+            })
+		)
+	);
+	euler.insert(
+		12,
+		(
+			String::from("What is the value of the first triangle number to have over five hundred divisors?"),
+			Box::new(|| {
+				let factors = |a| {
+					let mut r = vec![];
+					for i in 1..=a {
+						if a % i == 0 {
+							if !r.contains(&i) {
+								r.push(i);
+								if (a/i != i) {
+									r.push(a / i);
+								}
+							}
+							
+						}
+						if i*i > a {
+							break;
+						}
+					}
+					r.sort();
+					r
+				};
+				let mut r: u128 = 0;
+
+				let mut t = 1;
+				for i in 2.. {
+					let f = factors(t);
+					if f.len() > 500 {
+						r = t;
+						break;
+					}
+					t += i;
+				}
+
+				format!("{}", r)
+			})
 		)
 	);
 
