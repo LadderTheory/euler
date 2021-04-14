1c73179646f0e0041e923031344c4de4a158a5d1
Ladder Theory <brenton.ferullo@me.com>
2021-04-14 07:40:22 -0400

    added problem 17(unfinished). Started problem 18

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod bignum;
-
 use std::{collections::HashMap, io::{Write}};
 use num::{BigUint};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
@@ -437,6 +435,103 @@ fn main() {
 				}).collect::<Vec<u32>>().iter().sum::<u32>();
 
 				format!("{}", sum)
+			})
+		)
+	);
+	euler.insert(
+		17,
+		(
+			format!("UNFINISED!!!!! If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?"),
+			Box::from(|| {
+				let num_names = [
+					"one",
+					"two",
+					"three",
+					"four",
+					"five",
+					"six",
+					"seven",
+					"eight",
+					"nine",
+					"ten",
+					"eleven",
+					"twelve",
+					"thirteen",
+					"fourteen",
+					"fifthteen",
+					"teen",
+					"twenty",
+					"thirty",
+					"forty",
+					"fifty",
+					"sixty",
+					"seventy",
+					"eighty",
+					"ninety",
+					"hundred",
+					"thousand",
+				];
+
+				let name_lens = num_names.iter().map(|x| x.len() as u32).collect::<Vec<u32>>();
+
+				fn num_length(x: usize, arr: &Vec<u32>) -> u32 {
+					let mut r = 0;
+
+					if x <= 0 {}
+					else if x < 16 {
+						r = arr[x-1];
+					} else if x < 20 {
+						r += arr[15];
+						r += arr[x-11];
+
+						if x == 18 {
+							r = 8;
+						}
+					} else if x < 100 {
+						r += arr[14+(x/10)];
+						r += num_length(x%10, arr);
+					} else if x < 1000 {
+						r += arr[x/100];
+						r += arr[24];
+						let recurse = num_length(x%100, arr);
+						if recurse > 0 {
+							r += recurse + 3;//and
+						}
+					} else {
+						r += arr[0];
+						r += arr[25];
+						r += num_length(x % 1000, arr);
+					}
+
+					r
+				}
+
+				let mut sum = 0;
+
+				for i in 1..=1000 {
+					let l = num_length(i, &name_lens);
+					sum += l;
+					println!("{}: {}", i, l)
+				}
+
+				format!("{}", sum)
+			})
+		)
+	);
+	euler.insert(
+		18, 
+		(
+			format!("Find the maximum total from top to bottom of the triangle below:"),
+			Box::from(|| {
+				let triangle = vec![75, 95, 64, 17, 47, 82, 18, 35, 87, 10, 20, 04, 82, 47, 65, 19, 01, 23, 75, 03, 34, 88, 02, 77, 73, 07, 63, 67, 99, 65, 04, 28, 06, 16, 70, 92, 41, 41, 26, 56, 83, 40, 80, 70, 33, 41, 48, 72, 33, 47, 32, 37, 16, 94, 29, 53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14, 70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57, 91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31 , 04, 62, 98, 27, 23, 09, 70, 98, 73, 93, 38, 53, 60, 04, 23];
+
+				let printme = triangle
+								.iter()
+								.enumerate()
+								.fold("".to_string(), |acc, (i,x)| {
+									"".to_string()
+								});
+				format!("{:?}", printme)
 			})
 		)
 	);
